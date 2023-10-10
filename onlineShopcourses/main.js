b68c45dc928d5d8ea7de5a155be957c5b687b437
AlexMotley <piupiubang6@gmail.com>
2023-10-10 19:32:27 +0500

    Update

--- onlineShopcourses/main.js
+++ onlineShopcourses/main.js
@@ -1,24 +1,31 @@
 import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+// src/mocks/browser.js
+import { setupWorker } from 'msw'
+import { handlers } from './src/mocks/handlers'
 
-setupCounter(document.querySelector('#counter'))
+// This configures a Service Worker with the given request handlers.
+export const worker = setupWorker(...handlers)
+
+if (process.env.NODE_ENV === 'development') {
+  const { worker } = require('./mocks/browser.js')
+  worker.start()
+}
+
+const runApp = async () => {
+  switch (process.env.NODE_ENV) {
+    case "development":
+      await import("/src/mocks/browser.js")
+          .then(async ({ worker }) => {
+              await worker.start().then(() => {
+                  console.debug("App dev run")
+              })
+          })
+  }
+}
+
+runApp()
+  .catch((err) => {
+      console.error(err)
+  })
+
